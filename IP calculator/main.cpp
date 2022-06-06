cf7b712ad9f80a2fa70ab1ede021e1821a860b56
Chon33 <theforest153.0@gmail.com>
2022-06-06 18:35:25 +0200

    Add readme

--- IP calculator/main.cpp
+++ IP calculator/main.cpp
@@ -1,16 +1,13 @@
 /*
  * Network Calculator
-
- * Get mask, network, first host, last host
- * and broadcast from a given IP with mask.
+ *
+ * Get mask, network, first host, last host and broadcast from a given IP with mask.
  * You may input a file too.
-
+ *
  * IP/mask Ex: 10.3.56.100/8
-
- * File should contain one IP/mask (like the
- * avobe example) in each line.
-
-
+ * File must contain one IP/mask (like the example avobe) per line.
+ *
+ * 
  * Made by Daniel @ Github :)
  * https://github.com/Chon33/IP-calculator
 */
---
+++ README.md
@@ -0,0 +1,7 @@
+# IP-calculator
+
+Get mask, network, first host, last host and broadcast from a given IP with mask.  
+You may input a file too.  
+   
+IP/mask Ex: 10.3.56.100/8  
+File must contain one IP/mask (like the example avobe) per line.
